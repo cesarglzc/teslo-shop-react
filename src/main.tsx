aa1a0fc33ed8673321bc55a62c66113fa4faec23
cesarglzc <cesar.glzc91@gmail.com>
2026-04-19 19:53:11 -0600

    feat: initial commit

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { TesloShopApp } from './TesloShopApp'
+import './index.css'
+
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+
+    <TesloShopApp />
+    
+  </StrictMode>,
+)
