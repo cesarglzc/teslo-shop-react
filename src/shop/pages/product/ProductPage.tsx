ce6ed3a6203919e1b99ebcb47c715e12a2ea0490
cesarglzc <cesar.glzc91@gmail.com>
2026-05-05 02:07:02 -0600

    remove Button en ProductPage

--- src/shop/pages/product/ProductPage.tsx
+++ src/shop/pages/product/ProductPage.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button"
+// import { Button } from "@/components/ui/button"
 // import { useCounterStore } from "@/auth/store/auth.store"
 
 export const ProductPage = () => {
